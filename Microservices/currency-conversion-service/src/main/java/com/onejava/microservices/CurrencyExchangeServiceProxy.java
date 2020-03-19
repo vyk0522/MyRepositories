768ff503cfc45aa56c4dbde7c185cf4aff672c66
S <vivek.s@bt.com>
2020-03-19 22:41:05 +0530

    Zuul

--- Microservices/currency-conversion-service/src/main/java/com/onejava/microservices/CurrencyExchangeServiceProxy.java
+++ Microservices/currency-conversion-service/src/main/java/com/onejava/microservices/CurrencyExchangeServiceProxy.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name="currency-exchange-service", url = "localhost:8001")
-@FeignClient(name="currency-exchange-service") // URL should be removed when you use @RibbonClient
+//URL should be removed when you use @RibbonClient
+//@FeignClient(name="currency-exchange-service") 
+@FeignClient(name = "netflix-zuul-api-gateway-server")  // If you want API gateway to intercept the request
 @RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 	
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	//@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")  // In order to use API gateway
 	public CurrencyConversionBean retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
 
 }
