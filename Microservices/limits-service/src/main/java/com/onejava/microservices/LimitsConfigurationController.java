1e191b8a5ea5355b4366890f9a3cead52e073f81
S <vyk0522@gmail.com>
2020-03-21 19:58:50 +0530

    Cloud

--- Microservices/limits-service/src/main/java/com/onejava/microservices/LimitsConfigurationController.java
+++ Microservices/limits-service/src/main/java/com/onejava/microservices/LimitsConfigurationController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class LimitsConfigurationController {
 	
@@ -15,6 +17,17 @@ public class LimitsConfigurationController {
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfiguration() {
 		return new LimitConfiguration(propertiesConfiguration.getMinimum(), propertiesConfiguration.getMaximum());
+	}
+	
+	// http://localhost:8080/fault-tolerance
+	@GetMapping("/fault-tolerance")
+	@HystrixCommand(fallbackMethod = "fallBackRetrieveConfiguration")
+	public LimitConfiguration retrieveConfiguration() {
+		throw new RuntimeException("Not available");
+	}
+	
+	public LimitConfiguration fallBackRetrieveConfiguration() {
+		return new LimitConfiguration(0,0);
 	}
 
 }
